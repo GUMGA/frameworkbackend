2472b772316dbb4b9cfab12dbd39d92d597fac92
Munif <munifgebara@gmail.com>
2017-09-14 17:18:41 -0300

    1.5.20-SNAPSHOT

--- gumga-application/src/test/java/io/gumga/mysql/MysqlTest.java
+++ gumga-application/src/test/java/io/gumga/mysql/MysqlTest.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {MysqlSpringConfig.class})
-//@Ignore
+@Ignore
 public class MysqlTest extends AllDatabasesTest {
 
     public MysqlTest() {
--- gumga-application/src/test/java/io/gumga/postgres/PostgresTest.java
+++ gumga-application/src/test/java/io/gumga/postgres/PostgresTest.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {PostgresSpringConfig.class})
-//@Ignore
+@Ignore
 public class PostgresTest extends AllDatabasesTest {
 
     public PostgresTest() {
