52ccc62633f5aab3cf4d17a10e5d9a259779f2d2
Felipe Sabadini Facina <fsabadini@hotmail.com>
2020-01-06 15:58:55 -0300

    Correção no count com distinct no GQuery

--- gumga-application/src/main/java/io/gumga/application/GumgaGenericRepository.java
+++ gumga-application/src/main/java/io/gumga/application/GumgaGenericRepository.java
@@ -1042,7 +1042,7 @@ public class GumgaGenericRepository<T, ID extends Serializable> extends SimpleJp
 
     private Query createQueryCountWithGQuery(GQuery gQuery) {
         Boolean useDistinct = gQuery.useDistinct();
-        String query = (useDistinct ? "select distinct " : "select ") + " count(obj) FROM ".concat(entityInformation.getEntityName()).concat(" obj");
+        String query = (useDistinct ? "select count(distinct obj) " : "select count(obj)") + " FROM ".concat(entityInformation.getEntityName()).concat(" obj");
 
         String where = createWhere(gQuery);
 
